eac913e912c267efd82bd158da419418b3c4687c
Mark <Mark Yarchak>
2019-03-31 12:41:09 +0300

    load project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,81 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
+  parserOptions: {
+    sourceType: 'module',
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      jsx: true,
+      vue: true,
+    },
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  env: {
+    node: true,
+  },
+  plugins: [
+    'vue',
+  ],
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'import/no-unresolved': [
+      0,
+      {
+        commonjs: true,
+        amd: true,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        vue: 'never',
+        json: 'never',
+      },
+    ],
+    'function-paren-newline': [
+      'error',
+      'consistent',
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow:
+            [
+              '_id',
+              '_key',
+              '_isVue',
+              '__get',
+              '__typename',
+            ],
+      },
+    ],
+    'vue/attribute-hyphenation': [
+      'error',
+      'always',
+    ],
+    'vue/html-end-tags': 'error',
+    'vue/html-indent': [
+      'error',
+      2,
+    ],
+    'vue/html-self-closing': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': [
+      'error',
+      'double',
+    ],
+    'vue/order-in-components': 'error',
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+};
