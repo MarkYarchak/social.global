d1ef72dbc75ee0699e6e4120c70c6d020b3ef2b2
Mark <Mark Yarchak>
2019-03-31 19:36:48 +0300

    set project

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import state from './store/state';
+import getters from './store/getters';
+import mutations from './store/mutations';
+import actions from './store/actions';
+import mutationstypes from './store/mutations-types';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+  state,
+  getters,
+  mutations,
+  actions,
+  mutationstypes,
+  plugins: [createPersistedState()],
+});
